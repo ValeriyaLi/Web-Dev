292bbded0e72a3d6954d0f144579aff350bc8824
ValeriyaLi <98885105+ValeriyaLi@users.noreply.github.com>
2023-03-07 23:49:21 +0600

    Add files via upload

---
+++ lab6/src/album-photos/album-photos.component.ts
@@ -0,0 +1,45 @@
+import { Component , OnInit} from '@angular/core';
+import {Photo} from "../models";
+import {ActivatedRoute, Router} from "@angular/router";
+import {PhotoService} from "../photo.service";
+
+
+@Component({
+  selector: 'app-album-photos',
+  templateUrl: './album-photos.component.html',
+  styleUrls: ['./album-photos.component.css']
+})
+export class AlbumPhotosComponent implements OnInit{
+  photos: Photo[];
+  loaded: boolean;
+
+  constructor(
+    private route: ActivatedRoute,
+    private photoService: PhotoService,
+    private router: Router,
+
+  ) {
+    this.photos = [];
+    this.loaded = true;
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      this.getPhotos(id);
+    });
+  }
+
+  getPhotos(id: number) {
+    this.loaded = false;
+    this.photoService.getPhotos().subscribe((photos: Photo[]) => {
+      this.photos = photos.filter((photo) => photo.id === id);
+      this.loaded = true;
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['albums/:id']);
+  }
+
+}
